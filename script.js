9b6a1cb93e1d169b057acb2571aee301e4f41873
Marco Antônio de Decco Oliveira <marcodecco80@gmail.com>
2026-02-14 14:56:27 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode () {
+  const html = document.documentElement;
+
+  /*
+  if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  }
+
+  A linha abaixo faz exatamente o que a estrutura condicinal acima também faz, porém de forma mais sucinta
+  */
+  html.classList.toggle('light');
+
+
+  /*
+  Abaixo, pego a outra imagem para troca-lá entre os modos claro e escuro
+  */
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
